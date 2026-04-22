566dc0c4034b0a085eb3aca081d14b2e1ca7c8d8
Cristóbal Campos <ccampos@rebuss.com>
2026-04-22 03:25:36 +0000

    Registra nuevo especialista binocular

--- .devcontainer/custom.zsh
+++ .devcontainer/custom.zsh
@@ -4,3 +4,4 @@ export PATH="$HOME/.local/bin:$PATH"
 alias cristobal='cd ~/cristobal/especialistas/cristobal && claude'
 alias arquitecto='cd ~/cristobal/especialistas/arquitecto && claude'
 alias rebuss='cd ~/cristobal/especialistas/rebuss && claude'
+alias binocular='cd ~/cristobal/especialistas/binocular && claude'
